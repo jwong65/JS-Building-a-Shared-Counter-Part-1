e76ac26366fc1416846ad86fc027b472c13991ee
unknown <jason.wong6507@gmail.com>
2022-11-08 19:50:39 -0500

    Tried the new fetch() function

--- public/index.js
+++ public/index.js
@@ -1,9 +1,20 @@
-function main(){
+async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
 
-    let countValue = 0;
+    async function fetchfunction(){
+        console.log("Here")
+        return await fetch("http://localhost:9001/counter")
+    }
+
+    let response = await fetchfunction()
+    //let response = await fetch("http://localhost:9001/counter") can also work instead of just using a function returning fetch()
+    console.log(fetchfunction)
+    let obj = await response.json()
+    console.log(response)
+    console.log(obj)
+    let countValue = obj.value
 
     function increment(){
         countValue++;
